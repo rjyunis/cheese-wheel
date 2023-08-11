d09f3f0264af8cef371871963e54a1fa7ffc22c3
rjyunis <rjyunis@vt.edu>
2023-08-11 02:02:58 -0700

    Window titles now showing properly.

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,10 @@ use std::time::Duration;
 use mouce::Mouse;
 // use mouce::MouseActions;
 
+// Interact with windows
+use winapi::um::winuser::{GetCursorPos, WindowFromPoint, GetWindowTextW, GetWindowTextLengthW};
+use winapi::um::winnt::WCHAR;
+use winapi::shared::windef::POINT;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
@@ -25,6 +29,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
   mouse_manager.hook(Box::new(|event| {
     println!("you did{:?}", event);
+
+    // Retrieve Mouse Position and Window
+    let mut cursor_pos: POINT = POINT { x: 0, y: 0 };
+    unsafe { GetCursorPos(&mut cursor_pos) };
+    let window_handle = unsafe { WindowFromPoint(cursor_pos) };
+
+    // Retrieve window title
+    const BUFFER_SIZE: usize = 256;
+    let mut window_title: [WCHAR; BUFFER_SIZE] = [0; BUFFER_SIZE];
+    unsafe {
+        let title_length = GetWindowTextLengthW(window_handle) + 1;
+        GetWindowTextW(window_handle, window_title.as_mut_ptr(), title_length);
+    }
+
+    // Convert title to rust string
+    let window_title_string = String::from_utf16_lossy(&window_title);
+    println!("Current window title: {}", window_title_string);
+
   }))?;
   loop {
     // Call sleep to avoid heavy cpu load
