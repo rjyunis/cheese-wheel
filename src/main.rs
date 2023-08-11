10248a71e93e6a17c1851945f584706c83779a3c
rjyunis <rjyunis@vt.edu>
2023-08-11 04:13:33 -0700

    Cleaning up, adding comments

--- src/main.rs
+++ src/main.rs
@@ -18,23 +18,22 @@ use mouce::common::MouseEvent;
 use winapi::um::winuser::{GetCursorPos, WindowFromPoint, GetWindowTextW, GetWindowTextLengthW, SetLayeredWindowAttributes, LWA_ALPHA, LWA_COLORKEY, SetWindowLongPtrW, GWL_EXSTYLE, WS_EX_LAYERED, GetWindowLongPtrW, WS_EX_TRANSPARENT};
 use winapi::um::winnt::WCHAR;
 use winapi::shared::windef::{POINT, COLORREF};
-// use winapi::shared::windef::HWND;
 use winapi::shared::ntdef::LONG;
 use winapi::shared::minwindef::DWORD;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
+  // Get a mouse object
   let mut mouse_manager = Mouse::new();
 
+  // Confirm the program launched
   println!("hello, world");
 
-  // listen(mouse_manager);
-
+  // Set up a callback for mouse events
   mouse_manager.hook(Box::new(|event| {
 
+    // Check if this event was a left click
     if let MouseEvent::Press(mouce::common::MouseButton::Left) = event {
-
-      println!("you did{:?}", event);
 
       // Retrieve Mouse Position and Window
       let mut cursor_pos: POINT = POINT { x: 0, y: 0 };
@@ -49,7 +48,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
           GetWindowTextW(window_handle, window_title.as_mut_ptr(), title_length);
       }
 
-      // Convert title to rust string
+      // Convert title to rust string and print
       let window_title_string = String::from_utf16_lossy(&window_title);
       println!("Current window title: {}", window_title_string);
 
@@ -68,16 +67,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         new_style = current_style | WS_EX_LAYERED | WS_EX_TRANSPARENT;
       }
 
+      // Set whether window is layered, transparent
       unsafe { SetWindowLongPtrW(window_handle, GWL_EXSTYLE, (new_style as LONG).try_into().unwrap()) };
 
-      // Transparency parameters
-      let transparency_value = 80; // Adjust this value as needed (0 - fully transparent, 255 - fully opaque)
+      // Opaqueness parameters
+      let opaque_value = 80; // Adjust this value as needed (0 - fully transparent, 255 - fully opaque)
       let key_color = COLORREF::default(); // Specify a key color if needed
 
-      // Set window attributes for transparency
-      unsafe {
-        SetLayeredWindowAttributes(window_handle, key_color, transparency_value, LWA_ALPHA | LWA_COLORKEY);
-      }
+      // Set window attributes for opaqueness
+      unsafe { SetLayeredWindowAttributes(window_handle, key_color, opaque_value, LWA_ALPHA | LWA_COLORKEY) };
     }
   }))?;
   loop {
@@ -86,18 +84,3 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
   }
 
 }
-
-/*
- * TODO ask tim about this
-fn listen(mouse_manager: Box<dyn MouseActions>) -> Int,  {
-  let hook = mouse_manager.hook(Box::new(|event| {
-    println!("you did{:?}", event);
-  }))?;
-  loop {
-    // Call sleep to avoid heavy cpu load
-    sleep(Duration::from_secs(u64::max_value()));
-  }
-
-  return ();
-}
-*/
